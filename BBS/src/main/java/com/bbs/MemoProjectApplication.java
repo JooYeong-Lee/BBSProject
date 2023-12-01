3abbd016911b09813a60abafc8160ee5585e6f4e
JooYoung Lee <ljy4808@naver.com>
2023-12-01 17:44:10 +0900

    write.html 1차 디자인
    1. 폰트 수정
    2. 버튼 디자인
    해야 됨...

--- BBS/src/main/java/com/bbs/MemoProjectApplication.java
+++ BBS/src/main/java/com/bbs/MemoProjectApplication.java
@@ -1,4 +1,4 @@
-package com.memo;
+package com.bbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ BBS/src/main/java/com/bbs/controller/BBSController.java
@@ -0,0 +1,19 @@
+package com.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class BBSController {
+	@GetMapping("/main")
+    public String main() {
+        return "/bbs/main"; 
+    }
+	@GetMapping("/write")
+    public String write() {
+        return "/bbs/write"; 
+    }
+	@GetMapping("/post")
+    public String post() {
+        return "/bbs/post"; 
+    }
+}
