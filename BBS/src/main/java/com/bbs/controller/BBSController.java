4fbe6db5acf37ac682ccdaa50c7ac53aedf47db1
yofp1937 <112924147+yofp1937@users.noreply.github.com>
2023-12-04 19:18:50 +0900

    임시 회원가입, 로그인 화면 추가

--- BBS/src/main/java/com/bbs/controller/BBSController.java
+++ BBS/src/main/java/com/bbs/controller/BBSController.java
@@ -9,12 +9,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class BBSController {
+	
 	@GetMapping("/main")
     public String main(Model model) {
         return "bbs/main"; 
     }
+	
 	@GetMapping("/write")
     public String write(HttpServletRequest req) {
 		HttpSession session = req.getSession(false);
@@ -27,10 +30,35 @@ public class BBSController {
 			
         return "bbs/write"; 
     }
+	
 	@GetMapping("/post")
     public String post() {
         return "bbs/post"; 
     }
+	
+	@GetMapping("/join")
+	public String join() {
+		return "bbs/join";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "bbs/login";
+	}
+	
+	@PostMapping("/join_clear")
+	public String join_clear(@RequestParam String id,
+							 @RequestParam String pwd) {
+		
+		return "redirect:/main";
+	}
+	
+	@PostMapping("/login_clear")
+	public String login_clear(@RequestParam String id,
+							  @RequestParam String pwd) {
+		
+		return "redirect:/main";
+	}
 	
 	@PostMapping("/write_clear")
 	public String write_clear(@RequestParam String category,
