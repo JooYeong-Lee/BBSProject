f3432849ce99e50f36f47ef5cea8854b95567f77
yofp1937 <112924147+yofp1937@users.noreply.github.com>
2023-12-04 11:01:00 +0900

    화면 조정

--- BBS/src/main/java/com/bbs/controller/BBSController.java
+++ BBS/src/main/java/com/bbs/controller/BBSController.java
@@ -1,7 +1,11 @@
 package com.bbs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 @Controller
 public class BBSController {
 	@GetMapping("/main")
@@ -9,7 +13,15 @@ public class BBSController {
         return "bbs/main"; 
     }
 	@GetMapping("/write")
-    public String write() {
+    public String write(Model model, HttpServletRequest req) {
+		HttpSession session = req.getSession(false);
+		String userId;
+		if(session != null) { // 로그인이 상태면
+			userId = (String)session.getAttribute("user"); 
+		} else { // 로그인 상태가 아니면
+			userId = "Unknown User";
+		}
+			
         return "bbs/write"; 
     }
 	@GetMapping("/post")
--- BBS/src/main/java/com/bbs/user/userDB.java
+++ BBS/src/main/java/com/bbs/user/userDB.java
@@ -1,4 +1,4 @@
-package com.bbs;
+package com.bbs.user;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
